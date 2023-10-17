ddfb9c89fb6790d2f04ddb712067860d90122d34
alessandro <alessandrodamata6@gmail.com>
2023-10-17 19:25:31 -0300

    feat: add auth handler to header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+"use client"
+
+import { useSession } from "next-auth/react"
 
 export default function Home() {
+
+  const {data} = useSession()
   return (
    <div>
-    Home
+    {data?.user?.name}
    </div>
   )
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,25 @@
+"use client"
+
 import { Button } from "./button";
 import { Card } from "./card";
-import {HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon} from "lucide-react"
+import {HomeIcon, ListOrderedIcon, LogInIcon, LogOutIcon, MenuIcon, PercentIcon, ShoppingCartIcon} from "lucide-react"
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {signIn, signOut, useSession} from "next-auth/react"
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+
+  const {status, data} = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn()
+  }
+
+  const handleLogoutClick = async () => {
+    await signOut()
+  }
+
     return (
       <Card className="flex items-center justify-between p-[1.875rem]">
         <Sheet>
@@ -14,24 +30,73 @@ const Header = () => {
           </SheetTrigger>
 
           <SheetContent side={"left"}>
-            <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
-                <div className="mt-2 flex flex-col gap-3">
-                    <Button variant={"outline"} className="w-full justify-start gap-2">
-                        <LogInIcon size={16} /> Fazer Login
-                    </Button>
+            <SheetHeader className="text-left text-lg font-semibold">
+              Menu
+            </SheetHeader>
 
-                    <Button variant={"outline"} className="w-full justify-start gap-2">
-                        <HomeIcon size={16} /> Inicio
-                    </Button>
+            {status === "authenticated" && data?.user && (
+              <div className="flex flex-col">
+                <div className="flex items-center gap-2 py-4">
+                  <Avatar>
+                    <AvatarFallback>
+                      {data?.user?.name?.[0].toUpperCase()}
+                    </AvatarFallback>
 
-                    <Button variant={"outline"} className="w-full justify-start gap-2">
-                        <PercentIcon size={16} /> Ofertas
-                    </Button>
+                    {data?.user?.image && (
+                      <AvatarImage src={data?.user?.image} />
+                    )}
+                  </Avatar>
 
-                    <Button variant={"outline"} className="w-full justify-start gap-2">
-                        <ListOrderedIcon size={16} /> Catalogo
-                    </Button>
+                  <div className="flex flex-col">
+                      <p className="font-medium">{data?.user?.name}</p>
+                      <p className="text-sm opacity-75">Boas compras!</p>
+                  </div>
                 </div>
+                <Separator />
+              </div>
+            )}
+            <div className="mt-4 flex flex-col gap-3">
+              {status === "unauthenticated" && (
+                <Button
+                  onClick={handleLoginClick}
+                  variant={"outline"}
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon size={16} /> Fazer Login
+                </Button>
+              )}
+
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant={"outline"}
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon size={16} /> Fazer Logout
+                </Button>
+              )}
+
+              <Button
+                variant={"outline"}
+                className="w-full justify-start gap-2"
+              >
+                <HomeIcon size={16} /> Inicio
+              </Button>
+
+              <Button
+                variant={"outline"}
+                className="w-full justify-start gap-2"
+              >
+                <PercentIcon size={16} /> Ofertas
+              </Button>
+
+              <Button
+                variant={"outline"}
+                className="w-full justify-start gap-2"
+              >
+                <ListOrderedIcon size={16} /> Catalogo
+              </Button>
+            </div>
           </SheetContent>
         </Sheet>
 
